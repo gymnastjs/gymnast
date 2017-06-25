cce6d0215ee46e9c902b34e9ae2cc234ef87ce59
Oscar <obartra@gmail.com>
2017-06-25 08:38:27 -0700

    fix(deploy): Install before deploy (#114)
    Because we publish to gh-pages before deploying to npm, we need to re-run
    yarn since we deleted all files in the branch.

--- scripts/deploynpm.sh
+++ scripts/deploynpm.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-yarn build
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+
+yarn
+yarn build
 
 # It will fail if the same version is deployed (e.g. readme changes won't up the version).
 # It should fail because we don't want to redeploy in that case so we just hide the error
