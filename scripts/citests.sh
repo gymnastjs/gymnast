57dbe0c07edf4f7e891c1e819f0542b475e37b32
Oscar Bartra <obartra@gmail.com>
2017-06-26 19:09:34 -0700

    docs(readme): Add badges

--- scripts/citests.sh
+++ scripts/citests.sh
@@ -9,8 +9,10 @@ set -e
 echo "Running node $CIRCLE_NODE_INDEX"
 
 if [ "$CIRCLE_NODE_INDEX" -eq "0" ]; then
-  yarn lint       # Run linting tasks
-  yarn test:unit  # Run unit tests
+  yarn lint                    # Run linting tasks
+  yarn test:unit -- --coverage # Run unit tests
+  cat ./coverage/lcov.info | node_modules/.bin/codeclimate-test-reporter
+  cp -R coverage/* $CIRCLE_TEST_REPORTS # Copy test coverage reports for CircleCI
 else
-  yarn test:image # Run image comparison tests
+  yarn test:image              # Run image comparison tests
 fi
