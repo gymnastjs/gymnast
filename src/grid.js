b48929e984167deed1cb8927ad0b0b6bbf10abc5
Andrew Hansen <arahan@uw.edu>
2017-07-05 20:56:53 -0700

    fix(api): removed hoc wrappers (#157)
    HOCs made more sense when we wanted this library to act as wrappers for other components. The
    philosophy of the library has changed since then, and HOCs don't make sense any longer. They were
    causing bugs through the use of PureComponent + context

--- src/grid.js
+++ src/grid.js
@@ -1,12 +1,78 @@
+// @flow
 import React from 'react'
-import GridHOC from './grid.hoc'
+import PropTypes from 'prop-types'
+import { compact } from 'lodash'
+import { getSpacingClasses } from './utils'
+import type { Dev, AlignGrid, Justify, Size, Spacing } from './types'
+import Padding from './padding'
+import styles from './grid.css'
+import devStyles from './dev.css'
 
-class Grid extends React.PureComponent {
-  static displayName = 'Grid'
-
-  render() {
-    return <div {...this.props} />
-  }
+export type Props = {
+  dev?: Dev,
+  align?: AlignGrid,
+  children?: Element | Array<Element>,
+  className?: string,
+  justify?: Justify,
+  margin?: Spacing,
+  padding?: Spacing,
+  size?: Size,
 }
 
-export default GridHOC(Grid)
+export default class withGrid extends React.Component {
+  static defaultProps = {
+    margin: [],
+  }
+
+  static contextTypes = {
+    devMode: PropTypes.bool,
+  }
+
+  props: Props
+
+  render() {
+    const {
+      dev,
+      align,
+      children,
+      className,
+      justify,
+      margin,
+      padding,
+      size,
+      ...props
+    } = this.props
+
+    const classes = compact([
+      ...getSpacingClasses(margin),
+      className,
+      size && styles.col,
+      size && styles[`col-${String(size)}`],
+      dev &&
+        this.context.devMode &&
+        process.env.NODE_ENV !== 'production' &&
+        devStyles[`colors${String(dev)}`],
+      styles.grid,
+    ])
+    const offsetClasses = compact([
+      align && styles[`${align}Align`],
+      justify && styles[`${justify}Justify`],
+    ])
+
+    if (padding) {
+      return (
+        <div {...props} className={classes.join(' ')}>
+          <Padding direction={padding} className={offsetClasses.join(' ')}>
+            {children}
+          </Padding>
+        </div>
+      )
+    }
+
+    return (
+      <div {...props} className={[...classes, ...offsetClasses].join(' ')}>
+        {children}
+      </div>
+    )
+  }
+}
--- src/layout.js
+++ src/layout.js
@@ -1,12 +1,92 @@
+// @flow
 import React from 'react'
-import LayoutHOC from './layout.hoc'
+import PropTypes from 'prop-types'
+import { compact } from 'lodash'
+import { getSpacingClasses } from './utils'
+import type { Dev, Overflow, Fixed, Spacing, LayoutType } from './types'
+import styles from './layout.css'
+import devStyles from './dev.css'
 
-class Layout extends React.PureComponent {
-  static displayName = 'Layout'
-
-  render() {
-    return <div {...this.props} />
+function getLayout(layout: LayoutType): string {
+  switch (layout) {
+    case 'parent':
+      return styles.parent
+    case 'stretch':
+      return styles.stretch
+    default:
+      return styles.auto
   }
 }
 
-export default LayoutHOC(Layout)
+function getFixed(fixed: Fixed): string {
+  switch (fixed) {
+    case 'top':
+      return styles.fixedTop
+    case 'bottom':
+      return styles.fixedBottom
+    default:
+      return ''
+  }
+}
+
+function getOverflow(overflow: Overflow): string {
+  switch (overflow) {
+    case 'scrollbars':
+      return styles.overflow
+    default:
+      return ''
+  }
+}
+
+export default class withLayout extends React.Component {
+  static contextTypes = {
+    devMode: PropTypes.bool,
+  }
+
+  static childContextTypes = {
+    devMode: PropTypes.bool,
+  }
+
+  getChildContext() {
+    return {
+      devMode: this.props.devMode || this.context.devMode,
+    }
+  }
+
+  props: {
+    dev?: Dev,
+    devMode?: boolean,
+    className?: String,
+    fixed?: Fixed,
+    margin?: Spacing,
+    overflow?: Overflow,
+    type?: LayoutType,
+  }
+
+  render() {
+    const {
+      className,
+      fixed,
+      margin,
+      overflow,
+      type,
+      devMode,
+      dev,
+      ...props
+    } = this.props
+    const classes = compact([
+      className,
+      getFixed(fixed),
+      getLayout(type),
+      ...getSpacingClasses(margin),
+      getOverflow(overflow),
+      dev &&
+        (devMode || this.context.devMode) &&
+        process.env.NODE_ENV !== 'production' &&
+        devStyles[`colors${String(dev)}`],
+      styles.layout,
+    ])
+
+    return <div {...props} className={classes.join(' ')} />
+  }
+}
