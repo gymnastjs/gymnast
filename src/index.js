e9ddd9f7cdc3297873f95fd6beb9eb797d85b1c0
Andrew Hansen <arahan@uw.edu>
2017-07-05 20:44:38 -0700

    fix(api): removed hoc wrappers
    HOCs made more sense when we wanted this library to act as wrappers for other components. The
    philosophy of the library has changed since then, and HOCs don't make sense any longer. They were
    causing bugs through the use of PureComponent + context

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@
 import * as srcUtils from './utils'
 
 export { default as Grid } from './grid'
-export { default as GridHOC } from './grid.hoc'
 export { default as Layout } from './layout'
-export { default as LayoutHOC } from './layout.hoc'
 export { version } from '../version'
 export const utils = srcUtils
 
