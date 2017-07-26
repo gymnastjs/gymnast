5713ba100e43a02646d885f4c0602b788bf328d9
Andrew Hansen <arahan@uw.edu>
2017-07-26 00:42:09 -0700

    build(flow): strip content from *.css.flow files (#199)
    * build(flow): strip content from *.css.flow files

--- scripts/deploynpm.sh
+++ scripts/deploynpm.sh
@@ -7,6 +7,9 @@ echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 yarn
 yarn build
 
+# remove content from *.css.flow files so they are stubbed out and not parsed by flow
+node ./generateCssFlow.js
+
 # It will fail if the same version is deployed (e.g. readme changes won't up the version).
 # It should fail because we don't want to redeploy in that case so we just hide the error
 yarn semantic-release || exit 0
