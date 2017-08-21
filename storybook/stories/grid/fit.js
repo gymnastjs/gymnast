068ebc93866d6be40985be965a4861c137c52a45
Oscar <obartra@gmail.com>
2017-08-21 10:23:28 -0700

    docs(fit): Document fit example (#228)
    Documenr example on how to have a "fit" parent without overflowing content to the next line
    
    Closes https://github.com/obartra/reflex/issues/226

---
+++ storybook/stories/grid/fit.js
@@ -0,0 +1,35 @@
+// @flow
+import React from 'react'
+import { times } from 'lodash'
+import { number } from '@storybook/addon-knobs'
+import { Col, Grid } from 'reflex'
+import { RootLayout } from '../../shared'
+
+type ItemProps = {
+  text: string,
+}
+
+const Item = ({ text, ...props }: ItemProps) =>
+  <Col margin={[0.5]} {...props}>
+    <Grid padding={[0.5]} dev={1}>
+      {text}
+    </Grid>
+  </Col>
+
+export default () => {
+  const autoItems = number('Auto Items', 1, { range: true, min: 0, max: 5 })
+  const fitItems = number('Fit Items', 1, { range: true, min: 0, max: 5 })
+
+  return (
+    <RootLayout>
+      <Grid size="fit" justify="center" dev={2}>
+        {times(autoItems, i =>
+          <Item key={i} size="auto" text={`Auto ${i + 1}`} />
+        )}
+        {times(fitItems, i =>
+          <Item key={i} size="fit" text={`Fit ${i + 1}`} />
+        )}
+      </Grid>
+    </RootLayout>
+  )
+}
---
+++ storybook/stories/grid/fit.md
@@ -0,0 +1,3 @@
+# Fit
+
+When the parent size is set to `fit`, its children will always stay within one line. This effectively prevents the default autoflow but notice that the usage of `fit` already positions elements outside the grid.
