6cbb5798777b203705901c650739fa62c9170ef0
Andrew Hansen <ahansen@axon.com>
2017-09-28 19:44:45 -0700

    fix(withGrid): match export name to file name

--- src/reflex.js
+++ src/reflex.js
@@ -1,7 +1,10 @@
 // @flow
+import { type Props } from './withBase'
 import * as srcUtils from './utils'
 
-export { default as withCol } from './withGrid'
+export type GridProps = Props
+
+export { default as withGrid } from './withGrid'
 export { default as withBase } from './withBase'
 export { default as Base } from './base'
 export { default as Grid } from './grid'
@@ -13,6 +16,7 @@ export { default as Col } from './derived/col'
 export { default as Root } from './derived/root'
 export { default as Offset } from './derived/offset'
 export { default as initDevMode } from './dev'
+
 export type {
   Size,
   AlignGrid,
