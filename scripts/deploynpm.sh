2346806abe8ae99497c5621252438a408e3d48d7
Oscar <obartra@gmail.com>
2017-12-13 13:12:47 -0800

    ci(publish): Update semantic release syntax (#320)

--- scripts/deploynpm.sh
+++ scripts/deploynpm.sh
@@ -10,4 +10,4 @@ yarn build
 
 # It will fail if the same version is deployed (e.g. readme changes won't up the version).
 # It should fail because we don't want to redeploy in that case so we just hide the error
-./node_modules/.bin/semantic-release pre && npm publish && ./node_modules/.bin/semantic-release post || exit 0
+./node_modules/.bin/semantic-release || true
