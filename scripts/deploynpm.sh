89d351d0f9fdf421a531aa49a031553421b79ebd
Oscar <obartra@gmail.com>
2017-12-13 11:55:58 -0800

    fix(ci): Fix package publication (#316)

--- scripts/deploynpm.sh
+++ scripts/deploynpm.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 yarn
 rm -rf dist
 yarn build
 
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+
 # It will fail if the same version is deployed (e.g. readme changes won't up the version).
 # It should fail because we don't want to redeploy in that case so we just hide the error
-yarn semantic-release || exit 0
+./node_modules/.bin/semantic-release pre
+npm publish || exit 0
+./node_modules/.bin/semantic-release post
