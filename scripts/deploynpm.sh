da578e5d6d4e6f37246d47ffda4ada60d2c4982c
Oscar Bartra <obartra@taser.com>
2017-12-13 12:32:04 -0800

    fix(npmrc): Undo repositioning of npmrc

--- scripts/deploynpm.sh
+++ scripts/deploynpm.sh
@@ -2,15 +2,12 @@
 
 set -e
 
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 yarn
 rm -rf dist
 yarn build
 
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
-
 # It will fail if the same version is deployed (e.g. readme changes won't up the version).
 # It should fail because we don't want to redeploy in that case so we just hide the error
-./node_modules/.bin/semantic-release pre
-npm publish || exit 0
-./node_modules/.bin/semantic-release post
+./node_modules/.bin/semantic-release pre && npm publish && ./node_modules/.bin/semantic-release post || exit 0
