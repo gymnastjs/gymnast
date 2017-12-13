f006ee6ea4c72aaef20da82f2785c8f0164ae32a
Oscar Bartra <obartra@taser.com>
2017-12-13 12:21:15 -0800

    fix(npmrc): Undo replacing npmrc

--- scripts/deploynpm.sh
+++ scripts/deploynpm.sh
@@ -7,7 +7,7 @@ yarn
 rm -rf dist
 yarn build
 
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 # It will fail if the same version is deployed (e.g. readme changes won't up the version).
 # It should fail because we don't want to redeploy in that case so we just hide the error
