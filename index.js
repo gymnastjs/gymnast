928509342cb6ee1f5fb5ab842147e45832fcd04b
Oscar <obartra@gmail.com>
2017-12-15 10:56:01 -0800

    fix(npm): Correct npm targets (#327)
    * fix(npm): Correct npm targets
    
    - Remove `module` since although we can offer an ES6 build,
      it has additional (unlisted) dependencies: `cxs`, `preval`
    - Default to a new `index.js` file that decides which file
      to serve based on `NODE_ENV`
    - Preserve the `browser` section from `package.json` but
      make it point to the minified build by default

---
+++ index.js
@@ -0,0 +1,7 @@
+// @flow
+/* eslint-disable global-require */
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./dist/reflex.min.js')
+} else {
+  module.exports = require('./dist/reflex.js')
+}
