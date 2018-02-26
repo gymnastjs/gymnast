fcf0e8041cbca748cdb3400404ecbe244a6c4d51
Andrew Hansen <arahan@uw.edu>
2018-02-25 21:49:08 -0800

    update tests

--- src/core/core.spec.js
+++ src/core/core.spec.js
@@ -1,40 +1,22 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import asCore from './asCore'
+import { getCoreStyles } from './index'
 import defaults from '../defaults'
 
-describe('asCore', () => {
-  let wrapper
+describe('getCoreStyles', () => {
+  it('should return styles', () => {
+    const { styles } = getCoreStyles({ margin: 2 }, defaults)
 
-  it('should allow wrapping any element into a Core', () => {
-    const Span = asCore(() => <span />)
-    wrapper = mount(<Span />)
+    expect(styles).toMatchSnapshot()
+  })
 
-    expect(wrapper.html().includes('span')).toBe(true)
+  it('should return remaining props', () => {
+    const { props } = getCoreStyles({ margin: 2, foo: 'bar' }, defaults)
+
+    expect(props).toEqual({ foo: 'bar' })
   })
 
   it('should allow overriding base', () => {
-    const Grid = asCore('div')
+    const { styles } = getCoreStyles({ margin: 2, base: 10 }, defaults)
 
-    wrapper = mount(<Grid base={2} paddingTop={1} />)
-
-    expect(wrapper.html()).toContain('padding-top: 2px')
-  })
-
-  it('should use default base when no config or no base parameters are provided', () => {
-    const Grid = asCore('div')
-    const padding = 1
-
-    wrapper = mount(<Grid paddingTop={padding} />)
-
-    expect(wrapper.html()).toContain(
-      `padding-top: ${defaults.base * padding}px`
-    )
-  })
-
-  afterEach(() => {
-    if (wrapper) {
-      wrapper.unmount()
-    }
+    expect(styles).toMatchSnapshot()
   })
 })
