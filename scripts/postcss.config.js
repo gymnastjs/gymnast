970cd0d3583154c01bb93abfab2f3fbe619854d4
Oscar Bartra <obartra@gmail.com>
2018-08-19 06:34:20 -0700

    cleanup postcss config

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -1,17 +1,6 @@
 const cssImport = require('postcss-import')
 const cssNesting = require('postcss-nesting')
-const cssNext = require('postcss-cssnext')
-const cssNano = require('cssnano')
 
 module.exports = {
-  plugins: [
-    cssImport(),
-    cssNesting(),
-    cssNext({
-      features: {
-        autoprefixer: false,
-      },
-    }),
-    cssNano(),
-  ],
+  plugins: [cssImport(), cssNesting()],
 }
