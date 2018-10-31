b34610cfbe6dea9b8d63ff47242adee8b99b24c1
Oscar Bartra <obartra@taser.com>
2018-10-31 14:48:01 -0700

    address feedback

--- scripts/update-deps.sh
+++ scripts/update-deps.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
+# Update dependencies
 yarn upgrade --latest
+
+# Update flow definitions
+rm -rf flow-typed
 yarn flow-typed update-cache
-yarn flow-typed install --overwrite
+yarn flow-typed install --overwrite --skip
